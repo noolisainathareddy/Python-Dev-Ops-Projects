bcc9ff3db8ec17e645e257b802e5931e81427d34
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-12-15 20:16:56 -0600

    Added Day-01 folder

--- AWS-Integration/start.sh
+++ AWS-Integration/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=AKIATM343HXBA7S2V5GL
+export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-1
 
 
-uvicorn main:app --reload --host 127.0.0.1 --port 8000
+uvicorn main:app --reload --host 0.0.0.0 --port 8000
